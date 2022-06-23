4a81c9434e477a1caf62177325383190fa9cf2d2
amuskatt <102235034+amuskatt@users.noreply.github.com>
2022-06-22 22:05:12 -0400

    commit tree 2146d0975bb345c09c88ee06961e4749e4580a5e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(remark = "a hard worker"){
+            return `You are ${flair}${remark}${flair}!`
+
+        }
+ } 
+wrapAdjective("||")("a dedicated programmer")
